f2f63a221eb9e2ddb666280de7a77ff5b2dbb2b1
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-03-03 17:02:01 +0530

    added stack using LL

--- Stack.cpp
+++ Stack.cpp
@@ -4,19 +4,19 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-class Stack{
+class StackArr{
     int *stack, top, MAX;
 
     public:
 
-    Stack(){
+    StackArr(){
         cout<<"Enter the max size of the Stack: ";
         cin>>MAX;
         top = -1;
         stack = new int[MAX];
     }
 
-    Stack(int size){
+    StackArr(int size){
         MAX = size;
         top = -1;
         stack = new int[MAX];
@@ -62,7 +62,7 @@ class Stack{
         for(int i=top;i>=0;i--){
             cout<<stack[i];
             if(i == top)
-                cout<<" <- TOP";
+                cout<<" <-- TOP";
 
             cout<<endl;
         }
@@ -110,8 +110,124 @@ class Stack{
     }
 };
 
+
+
+
+class StackLL{
+    struct Node{
+        int data;
+        Node* next;
+    }*top;
+
+    public:
+
+    StackLL(){
+        top = NULL;
+    }
+
+    void push(int item){
+        if(top == NULL){
+            top = new Node;
+            if(top == NULL){
+                cout<<"Overflow\n";
+                return;
+            }
+            top->data = item;
+            top->next = NULL;
+        }
+
+        Node *temp = new Node;
+        if(temp == NULL){
+            cout<<"Overflow\n";
+            return;
+        }
+
+        temp->data = item;
+        temp->next = top;
+        top = temp;
+    }
+
+    int pop(){
+        if(top == NULL){
+            cout<<"Underflow\n";
+            return -1;
+        }
+
+        int item = top->data;
+        top = top->next;
+        return item;
+    }
+
+    int peek(){
+        if(top == NULL){
+            cout<<"Empty Stack.\n";
+            return -1;
+        }
+
+        return top->data;
+    }
+
+    void display(){
+        if(top == NULL){
+            cout<<"Empty Stack.\n";
+            return;
+        }
+
+        Node *temp = top;
+        cout<<temp->data<<" <-- TOP"<<endl;
+        temp = temp->next;
+        while(temp != NULL){
+            cout<<temp->data<<endl;
+            temp = temp->next;
+        }
+    }
+
+    void interface(){
+        int choice=0, item;
+
+        while(choice != -1){
+            cout<<"1) Push\n";
+            cout<<"2) Pop\n";
+            cout<<"3) Peek\n";
+            cout<<"4) Display\n";
+            cout<<"-1) Exit\n";
+
+            cout<<"Enter your choice: ";
+            cin>>choice;
+
+            switch(choice){
+                case 1:
+                        cout<<"Enter a number to push: ";
+                        cin>>item;
+                        push(item);
+                        break;
+
+                case 2:
+                        cout<<"Element that was popped: "<<pop()<<endl;
+                        break;
+
+                case 3:
+                        cout<<"The top element is: "<<peek()<<endl;
+                        break;
+                    
+                case 4:
+                        display();
+                        break;
+
+                case -1:
+                        break;
+
+                default:
+                        cout<<"Invalid choice!\n";
+            }
+        }
+    }
+};
+
+
+
 int main(){
-    Stack s;
+    StackLL s;
     s.interface();
 
     return 0;
