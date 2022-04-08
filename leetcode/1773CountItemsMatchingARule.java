c899878794ef8d2794522ba261e973c00936e1b2
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-04-08 22:48:29 +0530

    1773

---
+++ leetcode/1773CountItemsMatchingARule.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int count = 0;
+        for(int i=0;i<items.size();i++){
+            if((ruleKey.equals("type") && items.get(i).get(0).equals(ruleValue))||(ruleKey.equals("color") && items.get(i).get(1).equals(ruleValue))||(ruleKey.equals("name") && items.get(i).get(2).equals(ruleValue)))
+                count++;
+        }
+        return count;
+    }
+}
