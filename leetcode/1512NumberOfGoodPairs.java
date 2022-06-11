00c15638b60031ba5f7163499b4cd1480f2a9f69
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-06-11 14:39:29 +0530

    leetcode

--- leetcode/1512NumberOfGoodPairs.java
+++ leetcode/1512NumberOfGoodPairs.java
@@ -1,12 +1,49 @@
 class Solution {
-    public int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for(int i=0;i<nums.length;i++){
-            for(int j=i+1;j<nums.length;j++){
-                if(nums[i] == nums[j])
-                    count++;
-            }
-        }
-        return count;
+    
+	public static void main(String args[]){
+		int[] nums = {1,2,3,1,1,3};
+		System.out.println(numIdenticalPairs(nums));
+	}
+
+	public static int numIdenticalPairs(int[] nums) {
+        	HashMap<Integer, Integer> freq = new HashMap<Integer, Integer>();
+        	for(int i=0; i<nums.length;i++){
+			if(freq.containsKey(nums[i])){
+				freq.put(nums[i], freq.get(nums[i]) + 1);
+			}
+
+			else{
+				freq.put(nums[i], 1);
+			}
+		}
+
+		int sum = 0;
+        
+		for(int i:freq.values()){
+			sum += i*(i-1)/2;
+		}
+
+		return sum;
     }
 }
+
+/*
+Approach 1:
+    Two Nested loops brute force, time O(n^2)
+    
+    
+Approach 2:
+
+[1 2 3 1 1 3]
+
+Make a hashmap for freq of all values
+{
+    1: 3, // 3(2)/2 = 3
+    2: 1, // 1(0)/2 = 0
+    3: 2, // 2(1)/2 = 1
+}
+
+Result = Sum of values = 4
+
+time O(n)
+*/
