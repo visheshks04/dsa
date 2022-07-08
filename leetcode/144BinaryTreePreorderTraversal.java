19df70aa8e73c53f43ab3baca75bd92b77370213
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-07-08 12:02:23 +0530

    leetcode

---
+++ leetcode/144BinaryTreePreorderTraversal.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    private void preorder(ArrayList<Integer> l, TreeNode root){
+        if(root == null){
+            return;
+        }
+        
+        l.add(root.val);
+        preorder(l, root.left);
+        preorder(l, root.right);
+    }
+    
+    public List<Integer> preorderTraversal(TreeNode root) {
+        ArrayList<Integer> list = new ArrayList<>();
+        preorder(list, root);
+        
+        return list;
+    }
+}
