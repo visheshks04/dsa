c7280ae9d82f126678756f6914f621c0c5feccb2
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-07-14 21:44:19 +0530

    leetcode

---
+++ leetcode/704BinarySearch.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int hi=nums.length - 1;
+        int lo = 0;
+        int mid;
+        
+        while(hi >= lo){
+            mid = lo + (hi-lo)/2;
+            
+            if(nums[mid] == target)
+                return mid;
+            
+            else if(nums[mid] > target)
+                hi = mid-1;
+            
+            else
+                lo = mid+1;
+        }
+        
+        return -1;
+    }
+}
