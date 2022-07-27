5fededa765e6d2c29652acb19da4774422874a49
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2022-07-27 20:39:31 +0530

    leetcode

--- leetcode/231PowerOfTwo.java
+++ leetcode/231PowerOfTwo.java
@@ -1,5 +1,9 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        
+        if (n <= 0)
+            return false;
+        
         if(((n-1)&n) == 0)
             return true;
         return false;
