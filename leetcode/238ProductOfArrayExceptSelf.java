5d55fd50cd64d87984f41a6408c7beb246d8f162
Vishesh Kumar Singh <visheshkrsinghofficial@gmail.com>
2024-01-11 00:05:08 +0530

    leetcode

--- leetcode/238ProductOfArrayExceptSelf.java
+++ leetcode/238ProductOfArrayExceptSelf.java
@@ -1,27 +1,33 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] ans = new int[nums.length];
-        int[] fromLeft = new int[nums.length];
-        int[] fromRight = new int[nums.length];
-        
-        int leftProd = 1, rightProd = 1;
-        
-        for(int i=0;i<nums.length;i++){
-            leftProd = leftProd * nums[i];
-            fromLeft[i] = leftProd;
-            rightProd = rightProd * nums[nums.length-1-i];
-            fromRight[nums.length-1-i] = rightProd;
+        boolean oneZeroPresent = false;
+        boolean zeroResult = false;
+
+        int product = 1;
+
+        for (int i=0; i<nums.length; i++){
+            if (nums[i] == 0){
+                if (!oneZeroPresent) oneZeroPresent = true;
+                else zeroResult = true;
+            }
+            if(zeroResult) break;
+
+            if(nums[i] != 0) product *= nums[i];
         }
-        
-        for(int i=0;i<ans.length;i++){
-            if(i == 0)
-                ans[i] = fromRight[i+1];
-            else if(i == ans.length - 1)
-                ans[i] = fromLeft[i-1];
-            else
-                ans[i] = fromLeft[i-1] * fromRight[i+1];
+
+        int[] result = new int[nums.length];
+
+        if (zeroResult){
+            Arrays.fill(result, 0);
+            return result;
+        } 
+
+        for(int i=0; i<nums.length; i++){
+            if (nums[i] == 0) result[i] = product;
+            else if (oneZeroPresent) result[i] = 0;
+            else result[i] = product / nums[i];
         }
-        
-        return ans;
+
+        return result;
     }
 }
